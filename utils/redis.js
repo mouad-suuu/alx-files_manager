67e02b1d5cf1511534dd861b35d208ce9a962a51
mouad mennioui <moad.menniui@gmail.com>
2024-06-18 14:00:28 +0100

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -3,39 +3,74 @@ const redis = require("redis");
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
+    this.connected = false;
+
     this.client.on("error", (err) => {
-      console.error("Redis error:", err);
+      console.error(`Redis client error: ${err}`);
+    });
+
+    this.client.on("connect", () => {
+      console.log("Redis client connected to the server");
+      this.connected = true;
     });
   }
 
   isAlive() {
-    return this.client.connected;
+    return this.connected;
   }
 
   async get(key) {
+    await this.ensureConnection();
     return new Promise((resolve, reject) => {
       this.client.get(key, (err, value) => {
-        if (err) return reject(err);
-        resolve(value);
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
       });
     });
   }
 
   async set(key, value, duration) {
+    await this.ensureConnection();
     return new Promise((resolve, reject) => {
-      this.client.setex(key, duration, value, (err) => {
-        if (err) return reject(err);
-        resolve();
+      this.client.set(key, value, "EX", duration, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
       });
     });
   }
 
   async del(key) {
+    await this.ensureConnection();
     return new Promise((resolve, reject) => {
       this.client.del(key, (err) => {
-        if (err) return reject(err);
-        resolve();
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
       });
+    });
+  }
+
+  ensureConnection() {
+    return new Promise((resolve, reject) => {
+      if (this.connected) {
+        resolve();
+      } else {
+        this.client.once("connect", () => {
+          this.connected = true;
+          resolve();
+        });
+        this.client.once("error", (err) => {
+          reject(err);
+        });
+      }
     });
   }
 }
